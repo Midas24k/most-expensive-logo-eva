430047a3149a0e3b29961e6ca1f1fa43680d3211
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-01-17 14:37:21 -0500

    made changes to index.js functions logo.js functions shapes.js class shapes

--- index.js
+++ index.js
@@ -1,81 +1,84 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
 const path = require('path');
-const logo = require('lib/logo.js');
-const { Shapes, Triangle, Circle, Square} = require('./lib/shapes');
+const logoModule = require('./lib/logo.js');
+const { Shapes, Triangle, Circle, Square } = require('./lib/shapes');
 const questions = [
-    
+
     {
         type: 'input',
         name: 'text',
         message: 'Use three letters to abbriviate your companies name?',
 
     },
-   
+
     {
         type: 'input',
         name: 'color text',
         message: 'What color would you like text to be?',
     },
-    
+
     {
         type: 'list',
         name: 'shape',
         message: 'What shape would you like to use for a background of your logo',
         choices: [Triangle, Circle, Square]
-    },  
-    
+    },
+
     {
         type: 'input',
         name: 'color shape',
         message: 'What color would you like the shape to be?',
-    
+
     },
 
 ];
 
+// Will trigger the inquirer pkg using a switch statement to call ths shape selected with txt
 function init() {
     inquirer.prompt(questions)
         .then((data) => {
-        console.log(data)
-        console.log('Making your new logo');
-        switch (`${data.Shapes}`) {
-            case 'Triangle':
-                console.log('making a triangle')
-                const triangle = new Triangle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width)
-                fs.writeFile("lib/output/final_product", triangle.renderTriangle, (err) =>{
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('We did it boys, its a Triangle');
-                    }
+            console.log('Making your new logo');
+            switch (`${data.Shapes}`) {
+                case 'Triangle':
+                    console.log('making a triangle')
+                    const triangle = new Triangle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width)
+                    fs.writeFile("lib/output/final_product", triangle.renderTriangle, (err) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log('We did it boys, its a Triangle');
+                        }
+                    });
+                    break;
+                case 'Circle':
+                    console.log('ooh we drawing a circle');
+                    const circle = new Circle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.radius)
+                    fs.writeFile("lib/output/final_product", circle.renderCircle, (err) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log('We did it boys, its a Circle');
+                        }
+                    });
+                    break;
+                case 'Square':
+                    console.log('L7 thats right a square');
+                    const square = new Square(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width, data.width)
+                    fs.writeFile("lib/output/final_product", square.renderSquare, (err) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            console.log('We did it boys, its a Square');
+                        }
+                    });
+                    break;
+            }
 
-                });
-                break;
-            case 'Circle':
-                console.log('ooh we drawing a circle');
-                const circle = new Circle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.radius)
-                fs.writeFile("lib/output/final_product", circle.renderCircle, (err) =>{
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('We did it boys, its a Circle');
-                    }
-                });
-                break; 
-            case 'Square':
-                console.log('L7 thats right a square');
-                const square = new Square(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width, data.width)
-                fs.writeFile("lib/output/final_product", square.renderSquare, (err) => {
-                    if (err) {
-                        console.log(err);
-                    } else {
-                        console.log('We did it boys, its a Square');
-                    }
-                });
-        }
-        fs.writeFileSync(path.join(process.cwd(),'Mocklogo.svg'),logo({...data}))
-    });
+            // function to tie it all together
+            const logoResult = logoModule.createLogo(data);
+            fs.writeFileSync(path.join(process.cwd(), 'Mocklogo.svg'), logoResult)
+        });
 }
-
+// This will initialize the app
 init();

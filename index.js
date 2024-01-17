858df11771281d65ac44e08aafc288c58c7194b0
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-01-16 19:06:15 -0500

    started coding the index.js

--- index.js
+++ index.js
@@ -1,8 +1,41 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const path = require('lib/generatorshapes.js');
+const {logo} = require('lib/logo.js');
+const { Circle, Square, Triangle } = require('./lib/shapes');
 
 const questions = [
-    
+    {
+        type: 'input',
+        name: 'text',
+        message: 'Use three letters to abbriviate your companies name?',
 
-]
+    },
+    {
+        type: 'input',
+        name: 'color text',
+        message: 'What color would you like text to be?',
+    },
+    {
+        type: 'list',
+        name: 'shape',
+        message: 'What shape would you like to use for a background of your logo',
+        choices: [Triangle, Circle, Square]
+    },  
+    {
+        type: 'input',
+        name: 'color shape',
+        message: 'What color would you like the shape to be?',
+    
+    },
+
+];
+
+function init() {
+    inquirer.createPromptModule(questions).then((response) => {
+        console.log(response)
+        console.log('Making your new logo')
+        fs.writeFileSync(path.join(process.cwd(),'Mocklogo.svg',))
+    });
+}
+
+init();

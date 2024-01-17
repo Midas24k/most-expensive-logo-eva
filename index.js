4e72c0e476c8866ffd43547564775760c4d015b6
Midas24kMidas24k <137853877+Midas24k@users.noreply.github.com>
2024-01-16 22:18:07 -0500

    questions for inquirer work properly and made switch satatement for function on index.js

--- index.js
+++ index.js
@@ -1,26 +1,30 @@
 const inquirer = require('inquirer');
 const fs = require('fs');
-const {logo} = require('lib/logo.js');
-const { Circle, Square, Triangle } = require('./lib/shapes');
-
+const path = require('path');
+const logo = require('lib/logo.js');
+const { Shapes, Triangle, Circle, Square} = require('./lib/shapes');
 const questions = [
+    
     {
         type: 'input',
         name: 'text',
         message: 'Use three letters to abbriviate your companies name?',
 
     },
+   
     {
         type: 'input',
         name: 'color text',
         message: 'What color would you like text to be?',
     },
+    
     {
         type: 'list',
         name: 'shape',
         message: 'What shape would you like to use for a background of your logo',
         choices: [Triangle, Circle, Square]
     },  
+    
     {
         type: 'input',
         name: 'color shape',
@@ -31,10 +35,46 @@ const questions = [
 ];
 
 function init() {
-    inquirer.createPromptModule(questions).then((response) => {
-        console.log(response)
-        console.log('Making your new logo')
-        fs.writeFileSync(path.join(process.cwd(),'Mocklogo.svg',))
+    inquirer.prompt(questions)
+        .then((data) => {
+        console.log(data)
+        console.log('Making your new logo');
+        switch (`${data.Shapes}`) {
+            case 'Triangle':
+                console.log('making a triangle')
+                const triangle = new Triangle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width)
+                fs.writeFile("lib/output/final_product", triangle.renderTriangle, (err) =>{
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('We did it boys, its a Triangle');
+                    }
+
+                });
+                break;
+            case 'Circle':
+                console.log('ooh we drawing a circle');
+                const circle = new Circle(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.radius)
+                fs.writeFile("lib/output/final_product", circle.renderCircle, (err) =>{
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('We did it boys, its a Circle');
+                    }
+                });
+                break; 
+            case 'Square':
+                console.log('L7 thats right a square');
+                const square = new Square(data.fill, data.stroke, data.strokeWidth, data.text, data.textColor, data.width, data.width)
+                fs.writeFile("lib/output/final_product", square.renderSquare, (err) => {
+                    if (err) {
+                        console.log(err);
+                    } else {
+                        console.log('We did it boys, its a Square');
+                    }
+                });
+        }
+        fs.writeFileSync(path.join(process.cwd(),'Mocklogo.svg'),logo({...data}))
     });
 }
 
